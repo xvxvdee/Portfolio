2a660921d4eda8a7885ac830f7d4d678c812d9ce
xvxvdee <deandra.spikemadden@gmail.com>
2024-01-05 23:22:13 -0500

    Added in configurations for the Razor Pages

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -6,12 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings")); // This line loads MongoDB settings from configuration
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseDeveloperExceptionPage();
+
 app.Run();
