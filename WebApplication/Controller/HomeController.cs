e12213b99452658d53ffb2634f7c0cb17cf7d567
xvxvdee <deandra.spikemadden@gmail.com>
2024-01-05 23:20:00 -0500

    Handles view outputs for the pages in the home folder

---
+++ WebApplication/Controller/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using apiService.Services;
+
+namespace homeController.Controllers;
+
+[Route("Deandra")]
+
+public class HomeController : Controller{
+    private readonly ApiService apiService = new ApiService();
+
+    [HttpGet("about")]
+    public async Task<IActionResult> About(){
+        ViewData["Title"] = "About";
+        ViewData["Description"] = await this.apiService.GetAbout();
+        return View(); // Looks for Views/Home/About.cshtml
+    }
+}
